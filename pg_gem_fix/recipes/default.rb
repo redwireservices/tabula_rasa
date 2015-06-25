8b3030ccd6850ac558d6991b54594dc2ce181934
Nick Webb <nickw@redwireservices.com>
2015-06-24 22:57:58 -0700

    Remove postgres fix for now

--- pg_gem_fix/recipes/default.rb
+++ pg_gem_fix/recipes/default.rb
@@ -6,12 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-gem_package "pg" do
-  gem_binary "/opt/aws/opsworks/local/bin/gem"
-  options "--no-user-install --install-dir /opt/aws/opsworks/current/vendor/bundle/ruby/2.0.0"
-  action :install
-  version "0.18.2"
-end
+#gem_package "pg" do
+#  gem_binary "/opt/aws/opsworks/local/bin/gem"
+#  options "--no-user-install --install-dir /opt/aws/opsworks/current/vendor/bundle/ruby/2.0.0"
+#  action :install
+#  version "0.18.2"
+#end
 
 gem_package "ruby-augeas" do
   gem_binary "/opt/aws/opsworks/local/bin/gem"
