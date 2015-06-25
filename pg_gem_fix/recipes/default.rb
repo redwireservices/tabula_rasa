3d5f8e2fc2790f8586b8ca25b159c1f3feae3b68
Nick Webb <nickw@redwireservices.com>
2015-06-24 22:45:09 -0700

    Update for postges pg gem as well.

--- pg_gem_fix/recipes/default.rb
+++ pg_gem_fix/recipes/default.rb
@@ -6,12 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-chef_gem "pg" do
-  action :install
-  version "0.18.2"
-end
-
 gem_package "pg" do
+  gem_binary "/opt/aws/opsworks/local/bin/gem"
+  options "--no-user-install --install-dir /opt/aws/opsworks/current/vendor/bundle/ruby/2.0.0"
   action :install
   version "0.18.2"
 end
